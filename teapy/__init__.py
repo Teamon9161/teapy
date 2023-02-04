10ec319bbbf8eec850e83a66b1223fc4229d976f
Teamon9161 <1127972945@qq.com>
2023-02-04 17:38:31 +0800

    teapy 0.1.1

--- teapy/__init__.py
+++ teapy/__init__.py
@@ -1,6 +1,4 @@
 from .array_func import *
-
-# from_pd = from_pandas
 from .datadict import DataDict, from_pd
 from .teapy import PyExpr as Expr  # PyDataDict as DataDict,; from_pandas,
 from .teapy import arange, concat_expr, eval, full
@@ -9,3 +7,5 @@ from .teapy import parse_expr_list as asexprs
 from .teapy import timedelta
 from .teapy import where_ as where
 from .window_func import *
+
+__version__ = "0.1.1"
