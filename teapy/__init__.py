f7c3d3d1c3126a96fa6c9891c933ee97fa1ff8bf
Teamon9161 <1127972945@qq.com>
2023-06-12 13:38:16 +0800

    teapy 0.1.4

--- teapy/__init__.py
+++ teapy/__init__.py
@@ -1,7 +1,7 @@
 from .array_func import *
 from .datadict import DataDict, from_pd
 from .teapy import PyExpr as Expr
-from .teapy import arange, calc_single_ret, concat, eval, full
+from .teapy import arange, calc_ret_single, concat, eval, full
 from .teapy import parse_expr as asexpr
 from .teapy import parse_expr_list as asexprs
 from .teapy import timedelta
@@ -9,4 +9,4 @@ from .teapy import where_ as where
 from .window_func import *
 
 Expr.where = Expr.where_
-__version__ = "0.1.3"
+__version__ = "0.1.4"
