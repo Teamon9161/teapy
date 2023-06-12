58c12e6082909ece318dfd959a9866d8f54283f9
Teamon9161 <1127972945@qq.com>
2023-06-12 11:25:14 +0800

    fix version error

--- teapy/__init__.py
+++ teapy/__init__.py
@@ -6,6 +6,7 @@ from .teapy import parse_expr as asexpr
 from .teapy import parse_expr_list as asexprs
 from .teapy import timedelta
 from .teapy import where_ as where
+from .teapy import calc_single_ret
 from .window_func import *
 
 Expr.where = Expr.where_
